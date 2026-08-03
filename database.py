913781ed6ca7609f4615ce04ef678a462ab82cd2
Ez <ezy.misc@gmail.com>
2026-08-03 18:02:15 +0530

    Auth: create user model for auth and user_id for tasks

--- database.py
+++ database.py
@@ -1,10 +1,19 @@
 from sqlmodel import Field, SQLModel, create_engine, Session
 
+
+# model for tasks
 class Task(SQLModel, table=True):
     # table=True stores the class metadate in SQLModel.metadata
     id: int | None = Field(default=None,primary_key=True)
     name: str 
     description: str | None = None
+    user_id: int = Field(foreign_key="user.id")
+
+# model for users
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str
+    password: str
 
 sqlite_file_name = "database.sqlite"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
